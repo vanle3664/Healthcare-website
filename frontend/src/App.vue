8c75cc3a3e7b92208ad8a89e79336715b04d6c72
vân <01658635754levan@gmail.com>
2022-11-14 18:04:27 +0700

    Cart screen

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <Header/>
   <Menu/>
   <Content/>
@@ -7,11 +7,32 @@
 <script>
 import Header from './components/layout/TheHeader.vue'
 import Menu from './components/layout/TheMenu.vue'
-import Content from './components/layout/TheContent.vue'
+// import Content from './components/layout/TheContent.vue'
+import Cart from './components/layout/TheCart.vue'
 export default {
   name: 'App',
   components: {
     Header, Menu, Content
+  }
+}
+</script>
+
+<style scoped>
+  @import url('./styles/main.css');
+</style> -->
+
+<template>
+  <Header/>
+  <Cart/>
+</template>
+
+<script>
+import Header from './components/layout/TheHeader.vue'
+import Cart from './components/layout/TheCart.vue'
+export default {
+  name: 'App',
+  components: {
+    Header, Cart
   }
 }
 </script>
---
+++ frontend/src/components/layout/TheCart.vue
@@ -0,0 +1,73 @@
+<template lang="">
+    <div class="cart_page">
+        <div class="breaker_horizontal"> > Giỏ hàng</div>
+        <div class="all_content">
+            <div class="left_content">
+                <div class="products" v-for="product in productList" v-bind:key="product.name">
+                    <div class="prod_img">
+                        <img src="../../assets/product_img.webp">
+                    </div>
+                    <div class="products_info">
+                        <div class="top_info">
+                            <div class="product_name">{{product.name}}</div>
+                            <div class="product_num">
+                                <span class="minus" @click="handleMinus"> - </span>
+                                <span class="num">{{product.num}}</span>
+                                <span class="add" @click="handleAdd"> + </span>
+                            </div>
+                        </div>
+                        <div class="product_totalPrice">Đơn giá: {{product.num}} x {{product.price}} = {{product.num*product.price}}</div>
+                    </div>
+                </div>
+                <div class="delivery_info">Delivery</div>
+            </div>
+            <div class="breaker_vertical"></div>
+            <div class="right_content">
+                <div class="order_info">Order</div>
+            </div>        
+        </div>
+    </div>
+
+
+    
+</template>
+<script>
+export default {
+    name: 'TheCart',
+    data() {
+        return {
+            productList : [
+                {name: "Sữa Bột Abbott Glucerna Dinh Dưỡng Đặc Biệt Cho Người Đái Tháo Đường", num: 10, price: 10000}, 
+                {name: "Sữa Bột Abbott Glucerna Dinh Dưỡng Đặc Biệt Cho Người Đái Tháo Đường", num: 1, price: 30000},
+                {name: "Sữa Bột Abbott Glucerna Dinh Dưỡng Đặc Biệt Cho Người Đái Tháo Đường", num: 10, price: 10000}, 
+                {name: "Sữa Bột Abbott Glucerna Dinh Dưỡng Đặc Biệt Cho Người Đái Tháo Đường", num: 10, price: 10000}, 
+                {name: "Sữa Bột Abbott Glucerna Dinh Dưỡng Đặc Biệt Cho Người Đái Tháo Đường", num: 10, price: 10000}, 
+                {name: "Sữa Bột Abbott Glucerna Dinh Dưỡng Đặc Biệt Cho Người Đái Tháo Đường", num: 10, price: 10000}, 
+                {name: "Sữa Bột Abbott Glucerna Dinh Dưỡng Đặc Biệt Cho Người Đái Tháo Đường", num: 10, price: 10000}, 
+                {name: "Sữa Bột Abbott Glucerna Dinh Dưỡng Đặc Biệt Cho Người Đái Tháo Đường", num: 10, price: 10000}, 
+                {name: "Sữa Bột Abbott Glucerna Dinh Dưỡng Đặc Biệt Cho Người Đái Tháo Đường", num: 10, price: 10000}, 
+                {name: "Sữa Bột Abbott Glucerna Dinh Dưỡng Đặc Biệt Cho Người Đái Tháo Đường", num: 10, price: 10000}, 
+                {name: "Sữa Bột Abbott Glucerna Dinh Dưỡng Đặc Biệt Cho Người Đái Tháo Đường", num: 10, price: 10000}, 
+            ]
+        }
+    },
+    computed : {
+    },
+    methods: {
+        handleAdd(){
+            // console.log(this.$data[["productList"]][0])
+            console.log(this.productList)
+            let num = document.querySelector(".num");
+            num.innerText++;
+
+        },
+        handleMinus(){
+            let num = document.querySelector(".num");
+            if (num.innerText > 0) num.innerText--;
+        },
+    }
+}
+</script>
+<style scoped>
+    @import url('../../styles/layout/cart.css');
+</style>
