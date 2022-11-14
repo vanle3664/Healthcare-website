dff19b7417b7ee78b05b44f8483a8cdd52af0836
thule129 <baekhyunee129@gmail.com>
2022-11-15 02:32:14 +0700

    add component input and add vue router

---
+++ frontend/src/components/common/InputItem.vue
@@ -0,0 +1,28 @@
+<template lang="">
+    <div class="input-item">
+        <label>{{label}}</label>
+        <br>
+        <input :class="{'input-search':searchInput}">
+        <div class="search-icon" v-if="searchInput">
+            <i class="fa-solid fa-magnifying-glass"></i>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'InputItem',
+    props: {
+        label:{
+            type: String,
+            default: null
+        },
+        searchInput: {
+            type: Boolean,
+            default: false
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
