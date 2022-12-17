608fcff14ab8c1a4c95230eabf4e01b473d2d749
bva <hochochoc999@gmail.com>
2022-12-18 01:43:30 +0700

    add intent about symptoms

--- chatbot/actions/actions.py
+++ chatbot/actions/actions.py
@@ -1,27 +1,44 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
-# from typing import Any, Text, Dict, List
-#
 # from rasa_sdk import Action, Tracker
 # from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
+# from typing import Any, Text, Dict, List
+# from rasa_sdk.events import SlotSet
+
+# import logging
+# logger = logging.getLogger(__name__)
+# logging.basicConfig(level='DEBUG')
+# logger.info("LOG")
+
+# class PredictAction(Action):
+
+#     def name(self):
+#         return "action_chan_doan"
+
+#     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#         print('zap', tracker.slots)
+#         symptoms = tracker.slots.get('symptoms')
+#         print(symptoms)
+#         mess="Các triệu chứng của bạn là " + symptoms
+#         dispatcher.utter_message(text=mess)
 #         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk.events import SlotSet
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionNhacTrieuChung(Action):
+
+    def name(self) -> Text:
+        return "action_nhac_trieu_chung"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        symptoms = tracker.get_slot("symptoms")
+        if not symptoms:
+            dispatcher.utter_message(text="I don't know your shirt size.")
+        else:
+            dispatcher.utter_message(text=f"Your shirt size is {symptoms}!")
+        return []
