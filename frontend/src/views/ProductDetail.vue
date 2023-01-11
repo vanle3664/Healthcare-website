9272dd09a79e76b734f40e23a7c5ab29ebc94ad4
thule129 <baekhyunee129@gmail.com>
2023-01-11 19:02:37 +0700

    update product detail page

--- frontend/src/views/ProductDetail.vue
+++ frontend/src/views/ProductDetail.vue
@@ -1,15 +1,29 @@
 <template>
-    <div class="product-detail">
+    <div class="product-detail" v-if="product!=null">
         <div class="product-images-info">
             <div class="slideshow-container">
-                <SlideShow/>
+                <SlideShow
+                    :images="images"
+                />
             </div>
             <div class="product-info">
-                <div class="product-desc-1 scrollable">
+                <div class="product-desc-1">
                     <div class="title">
                         {{ product.product_name }}
                     </div>
-                    <p>{{product.description}}</p>
+                    <div class="price"><span>{{ product.price }}</span>VND/sản phẩm</div>
+                    <div>
+                        <i class="fa-regular fa-copyright"></i>
+                        {{ product.brand }}
+                    </div>
+                    <div>
+                        <i class="fa-solid fa-check"></i>
+                        {{ product.quantity_available }} sản phẩm có sẵn
+                    </div>
+                    <div>
+                        <i class="fa-solid fa-check"></i>
+                        Cam kết sản phẩm chính hãng
+                    </div>
                 </div>
                 <div class="product-options">
                     <NumberAdjust/>
@@ -27,9 +41,10 @@
         </div>
         <div class="product-desc-2 scrollable">
             <div class="title">Mô tả sản phẩm</div>
-            <p>{{ product.description }}</p>
+            <div ref="productDetail"></div>
         </div>
         <div class="products-grid-container">
+            <div class="title">Sản phẩm tương tự</div>
             <GridProducts/>
         </div>
     </div>
@@ -48,10 +63,12 @@ export default{
     data(){
         return{
             product: null,
+            images: [],
         }
     },
     created() {
         this.getProductDetail()
+
     }, 
     methods: {
         async getProductDetail(){
@@ -60,18 +77,23 @@ export default{
                 method: 'GET',
                 redirect: 'follow'
                 };
-            // const url = `http://127.0.0.1:8000/api/products/${productId}`
-            // fetch(url, requestOptions)
-            //     .then(res=>res.json())
-            //     .then(res=>{
-            //         this.product = res[0]
-                    
-            //     })
-                fetch(`http://127.0.0.1:8000/api/products/${productId}`, requestOptions)
-                .then(response => response.json())
-                .then(res=>this.product=res[0])
-                .then(result => console.log(result))
-                .catch(error => console.log('error', error));
+
+            await fetch(`http://127.0.0.1:8000/api/products/${productId}`, requestOptions)
+            .then(response => response.json())
+            .then(res=>{
+                this.product=res[0]
+                console.log(this.product)
+                this.images.push(this.product.product_image)
+                this.getProductDescription()
+            })
+            .catch(error => console.log('error', error));
+        },
+        getProductDescription(){
+            this.$nextTick(()=>{
+                console.log(this.$refs)
+            this.$refs.productDetail.insertAdjacentHTML('afterbegin', this.product.description)
+            })
+            
         }
     }                                                                                                 
 }
