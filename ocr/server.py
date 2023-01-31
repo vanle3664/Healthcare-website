8f001cac125f894187d5c89d2fe0fce0301828d3
vân <01658635754levan@gmail.com>
2023-01-31 13:28:40 +0700

    speed up + change response format

--- ocr/server.py
+++ ocr/server.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Response
 from paddleocr import PaddleOCR  
 from tensorflow.keras.utils import img_to_array
 from matplotlib import pyplot as plt 
 import cv2 
 import os 
+import numpy as np
 from io import BytesIO
 from PIL import Image
 
@@ -16,11 +17,9 @@ async def predict_api(file: UploadFile = File(...)):
         return "Image must be jpg or png format!"
     image = Image.open(BytesIO(await file.read()))
     image = img_to_array(image)
-    saved_image = cv2.imwrite("saved_image.png", image)
 
     ocr_model = PaddleOCR(lang='en')
-    result = ocr_model.ocr(saved_image, cls=True)
-
+    result = ocr_model.ocr(image, cls=True)
     print(result)
 
     final = []
@@ -29,8 +28,12 @@ async def predict_api(file: UploadFile = File(...)):
         for r in res:
             final.append(r[:][:][1])
     
-    os.remove("saved_image.jpg")
-    return final
+    max_point = -1
+    for i in range(0, len(final)):
+        if final[i][1] > max_point:
+            drug = final[i][0]
+    data = drug.split(" ")[0]
+    return Response(content=data, media_type="application/xml") 
 
 @app.post("/test")
 async def predict_api1():
